e06cbd2fa6211f8d668bdc66368b9087adf7982c
dms19fahim <dms19fahim@gmail.com>
2026-01-12 22:45:17 +0600

    Variables added

--- PHP-File/main.php
+++ PHP-File/main.php
@@ -1,3 +1,6 @@
 <?php
     echo "Hello, World!";
+    $myName = "Abdul Fahim";
+    $myProfession = "Back-End Web Developer";
+    echo "\nMy name is " . $myName . " and I am a " . $myProfession . ".";
 ?>
