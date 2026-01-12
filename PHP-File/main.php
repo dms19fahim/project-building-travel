7c50965255bd6bb010fe603643104fe28a23ca9a
dms19fahim <dms19fahim@gmail.com>
2026-01-12 22:49:22 +0600

    HTML Structure including

--- PHP-File/main.php
+++ PHP-File/main.php
@@ -1,6 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
+
+    echo "<h1>My first PHP Program</h1>";
     echo "Hello, World!";
+
     $myName = "Abdul Fahim";
     $myProfession = "Back-End Web Developer";
     echo "\nMy name is " . $myName . " and I am a " . $myProfession . ".";
 ?>
+
+</body>
+</html>
